2623efd79f656e53a227eded7df7c55a15f7be4a
ppanagiotaros <ppanagiotaros@sjnma.org>
2017-03-08 10:58:59 -0600

    dice are rolling

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+
+
+
+userinput = raw_input("Lets Play Dice! Grab a partner and type 'go' to start...")
+if userinput == "go":
+	print(dice1)
+	print(dice2)
+
+	
