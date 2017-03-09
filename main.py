804a00a36c45b7ab6e3bbb8c9adff9767b824d78
ppanagiotaros <ppanagiotaros@sjnma.org>
2017-03-09 10:39:17 -0600

    determining winner

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from random import randint
 dice1 = randint(1,6)
 dice2 = randint(1,6)
-
+p1win = 0
+p2win = 0
 
 
 
@@ -9,5 +10,11 @@ userinput = raw_input("Lets Play Dice! Grab a partner and type 'go' to start..."
 if userinput == "go":
 	print(dice1)
 	print(dice2)
+
+	if dice1>dice2:
+		print("player1 wins the round")
+
+	else:
+		print("player2 wins the round")
 
 	
