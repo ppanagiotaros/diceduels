820b64e0f0c50991a4de7181497f33be7e48eb34
ppanagiotaros <ppanagiotaros@sjnma.org>
2017-03-10 12:11:44 -0600

    got score working

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from random import randint
-dice1 = randint(1,6)
-dice2 = randint(1,6)
-p1win = 0
-p2win = 0
+from random import randrange
+
+p1 = 0
+p2 = 0
 
 
-
-userinput = raw_input("Lets Play Dice! Grab a partner and type 'go' to start...")
-if userinput == "go":
-	print(dice1)
-	print(dice2)
-
-	if dice1>dice2:
-		print("player1 wins the round")
-
-	else:
-		print("player2 wins the round")
-
+while p1 != 3 or p2 != 3:
 	
+	dice1 = randrange(1,6)
+	dice2 = randrange(1,6)
+
+	userinput = raw_input("Lets Play Dice! Grab a partner and type 'go' to start...")
+	if userinput == "go":
+		print(dice1)
+		print(dice2)
+
+		if dice1>dice2:
+			print("player1 wins the round")
+			p1 = p1 + 1
+
+		if dice1<dice2:
+			print("player2 wins the round")
+			p2 = p2 + 1
+
+		if dice1==dice2:
+			print("draw")
+
+		if p2 == 3:
+			print("Player 2 Wins!!!")
+
+		if p1 == 3:
+			print("Player 1 Wins!!!")
+
